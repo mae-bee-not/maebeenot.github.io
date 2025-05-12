ec348bce4ce3e739aa26f08011a0fe0cd8b29ed0
Mae <mae.bee.not1023@gmail.com>
2025-05-12 10:29:46 -0400

    WORK IN PROGRESS

--- scripts/config.js
+++
@@ -1,6 +0,0 @@
-// please don't steal my api key :3
-const config = {
-    client_id: 'c138f86f64164cc796b0ee84c76c90a1',
-    client_secret: 'a545ede2efce4a938c25606f6cf03e41',
-    refresh_token: 'AQDonYKxOg1fGaYowIvuowncn6li4GXpSRUhZpU2xO-TlE-pcgBjbZT1ooWuSQDsuXC3TFsEH9NpYQoghg0HqLaVlCHnZrwoiBDuSPd1Ruj1MrR8VxGini-K4MO4IJmMF8Q'
-};
